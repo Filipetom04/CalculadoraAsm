93392f1f1a62332663dc2cfb628a42168f3afad6
Filipetom04 <142317897+Filipetom04@users.noreply.github.com>
2025-01-31 14:59:37 -0400

    atualização após adicionar a biblioteca menu

--- main.asm
+++ main.asm
@@ -1,41 +1,45 @@
 section .data
-    tit      db  10, '-----------------',10, '| Calculadora |',10 ,0
-    ltit     equ $ - tit
-    obVal1   db  10,'Valor 1:',0
-    lobVal1  equ $ - obVal1
-    obVal2   db  10,'Valor 2:',0
-    lobVal2  equ $ - obVal2
-    opc1     db  10,'1. Adicionar',0
-    lopc1    equ $- opc1
-    opc2     db  10,'2. Subtrair',0
-    lopc2    equ $- opc2
-    opc3     db  10,'3. Multiplicar',0
-    lopc3    equ $- opc3
-    opc4     db  10,'4. Dividir',0
-    lopc4    equ $- opc4
-    msgOpc   db  10,'Deseja Realizar?',0
-    lmsgOpc  equ $- msgOpc
-    msgErro  db  10,'Valor da opção invalida!!',0
-    lmsgErro equ $- msgErro
-    msgDivZero db 10,'Erro: Divisão por zero!',0
-    lmsgDivZero equ $- msgDivZero
-    msgSubNeg  db  10,'Erro: Subtração resulta em valor negativo!',0
-    lmsgSubNeg equ $- msgSubNeg
-    nLinha   db 10,0
-    lLinha   equ $ - nLinha
-    result_msg db 10, 'Resultado: ', 0
+    tit         db  10, '-----------------', 10, '| Calculadora |', 10, 0
+    ltit        equ $ - tit
+    obVal1      db  10, 'Valor 1:', 0
+    lobVal1     equ $ - obVal1
+    obVal2      db  10, 'Valor 2:', 0
+    lobVal2     equ $ - obVal2
+    opc1        db  10, '1. Adicionar', 0
+    lopc1       equ $ - opc1
+    opc2        db  10, '2. Subtrair', 0
+    lopc2       equ $ - opc2
+    opc3        db  10, '3. Multiplicar', 0
+    lopc3       equ $ - opc3
+    opc4        db  10, '4. Dividir', 0
+    lopc4       equ $ - opc4
+    msgOpc      db  10, 'Deseja Realizar?', 0
+    lmsgOpc     equ $ - msgOpc
+    msgErro     db  10, 'Valor da opção invalida!!', 0
+    lmsgErro    equ $ - msgErro
+    msgDivZero  db  10, 'Erro: Divisão por zero!', 0
+    lmsgDivZero equ $ - msgDivZero
+    msgSubNeg   db  10, 'Erro: Subtração resulta em valor negativo!', 0
+    lmsgSubNeg  equ $ - msgSubNeg
+    nLinha      db  10, 0
+    lLinha      equ $ - nLinha
+    result_msg  db  10, 'Resultado: ', 0
     lresult_msg equ $ - result_msg
 
 section .bss
-    opc      resb 2
-    num1     resb 10
-    num2     resb 10
-    result   resb 10
+    opc         resb 2
+    num1        resb 10
+    num2        resb 10
+    result      resb 10
 
 section .text
     global _start
     extern string_to_int
     extern int_to_string
+    extern adicionar
+    extern subtrair
+    extern multiplicar
+    extern dividir
 
 _start:
     ; Exibir o título
@@ -118,13 +122,13 @@ _start:
 
     ; Verificar a operação escolhida
     cmp rax, 1
-    je adicionar
+    je .fazer_adicao
     cmp rax, 2
-    je subtrair
+    je .fazer_subtracao
     cmp rax, 3
-    je multiplicar
+    je .fazer_multiplicacao
     cmp rax, 4
-    je dividir
+    je .fazer_divisao
 
     ; Se a operação for inválida, exibir mensagem de erro
     mov rax, 1          ; sys_write
@@ -134,101 +138,104 @@ _start:
     syscall
     jmp saida
 
-adicionar:
+.fazer_adicao:
     ; Converter num1 para inteiro
     mov rsi, num1
     call string_to_int
-    mov rbx, rax        ; rbx = num1
+    mov rdi, rax        ; rdi = num1
 
     ; Converter num2 para inteiro
     mov rsi, num2
     call string_to_int
-    add rbx, rax        ; rbx = num1 + num2
+    mov rsi, rax        ; rsi = num2
+
+    ; Chamar a função adicionar da biblioteca calcular
+    call adicionar      ; rax = num1 + num2
 
     ; Exibir o resultado
-    mov rdi, result       ;endereço do buffer resultado
-    mov rax, rbx
+    mov rdi, result     ; endereço do buffer resultado
     call int_to_string
     call mostrar_valor
     jmp saida
 
-subtrair:
+.fazer_subtracao:
     ; Converter num1 para inteiro
     mov rsi, num1
     call string_to_int
-    mov rbx, rax        ; rbx = num1
+    mov rdi, rax        ; rdi = num1
 
     ; Converter num2 para inteiro
     mov rsi, num2
     call string_to_int
+    mov rsi, rax        ; rsi = num2
 
-    cmp rbx, rax          ; Comparar num1 com num2 (rbx - rax)
-    jl subtracao_negativa  ; Se num1 < num2, pular para subtracao_negativa
+    ; Verificar se o resultado será negativo
+    cmp rdi, rsi
+    jl .subtracao_negativa
 
-    sub rbx, rax        ; rbx = num1 - num2
+    ; Chamar a função subtrair da biblioteca calcular
+    call subtrair       ; rax = num1 - num2
 
     ; Exibir o resultado
-    mov rdi, result      ;endereço do buffer resultado
-    mov rax, rbx
+    mov rdi, result     ; endereço do buffer resultado
     call int_to_string
     call mostrar_valor
     jmp saida
 
-subtracao_negativa:
+.subtracao_negativa:
     ; Exibir mensagem de erro de subtração negativa
     mov rax, 1          ; sys_write
     mov rdi, 1          ; stdout
-    mov rsi, msgSubNeg ; mensagem de erro
+    mov rsi, msgSubNeg  ; mensagem de erro
     mov rdx, lmsgSubNeg ; tamanho da mensagem
     syscall
     jmp saida
 
-multiplicar:
+.fazer_multiplicacao:
     ; Converter num1 para inteiro
     mov rsi, num1
     call string_to_int
-    mov rbx, rax        ; rbx = num1
+    mov rdi, rax        ; rdi = num1
 
     ; Converter num2 para inteiro
     mov rsi, num2
     call string_to_int
-    imul rbx, rax       ; rbx = num1 * num2
+    mov rsi, rax        ; rsi = num2
+
+    ; Chamar a função multiplicar da biblioteca calcular
+    call multiplicar    ; rax = num1 * num2
 
     ; Exibir o resultado
-    mov rdi, result      ;endereço do buffer resultado
-    mov rax, rbx
+    mov rdi, result     ; endereço do buffer resultado
     call int_to_string
     call mostrar_valor
     jmp saida
 
-dividir:
+.fazer_divisao:
     ; Converter num1 para inteiro
     mov rsi, num1
     call string_to_int
-    mov rbx, rax        ; rbx = num1
+    mov rdi, rax        ; rdi = num1
 
     ; Converter num2 para inteiro
     mov rsi, num2
     call string_to_int
-    cmp rax, 0          ; Verificar se o divisor é zero
-    je divisao_por_zero ; Se for zero, exibir erro
+    mov rsi, rax        ; rsi = num2
 
-    ; Realizar a divisão
-    mov rcx, rax        ; Movendo o divisor para rcx
-    mov rax, rbx        ; Movendo o dividendo para rax
-    xor rdx, rdx        ; Limpar rdx para a divisão
-    idiv rcx           ; rax = rax / rcx (quociente em rax, resto em rdx)
+    ; Verificar se o divisor é zero
+    cmp rsi, 0
+    je .divisao_por_zero
 
-    mov rbx, rax        ; rbx = resultado da divisão
+    ; Chamar a função dividir da biblioteca calcular
+    call dividir        ; rax = num1 / num2
 
     ; Exibir o resultado
-    mov rdi, result      ;endereço do buffer resultado
-    mov rax, rbx
+    mov rdi, result     ; endereço do buffer resultado
     call int_to_string
     call mostrar_valor
     jmp saida
 
-divisao_por_zero:
+.divisao_por_zero:
     ; Exibir mensagem de erro de divisão por zero
     mov rax, 1          ; sys_write
     mov rdi, 1          ; stdout
@@ -266,10 +273,15 @@ mostrar_valor:
     mov rdx, 10         ; tamanho máximo do buffer de resultado
     mov rcx, result     ; ponteiro inicial da string do resultado
 .contar_caracteres:
-    cmp byte [rcx], 0 ; compara o byte apontado por rcx com 0
+    cmp byte [rcx], 0   ; compara o byte apontado por rcx com 0
     je .fim_contagem    ; se for 0, vai para o fim_contagem
     inc rcx             ; incrementa o ponteiro rcx
     inc rdx             ; incrementa o tamanho do buffer
+    jmp .contar_caracteres
+.fim_contagem:
+    sub rdx, 1          ; retira 1 do tamanho do buffer para eliminar o caractere nulo
+    syscall
+    ret
     jmp .contar_caracteres
 .fim_contagem:
     sub rdx,1          ;retira 1 do tamanho do buffer para eliminar o caractere nulo
